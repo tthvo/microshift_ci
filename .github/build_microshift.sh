d3098467c18f73afe7ab52f96efab14bc35bcf37
Thuan Vo <thvo@redhat.com>
2023-07-25 19:07:11 -0400

    fix go installation

--- .github/build_microshift.sh
+++ .github/build_microshift.sh
@@ -8,16 +8,6 @@ VERSION=${VERSION:-"4.13.0-rc-8"}
 
 install_build_deps() {
     sudo apt -y install build-essential curl libgpgme-dev pkg-config libseccomp-dev
-    # Install golang
-    wget https://golang.org/dl/go1.19.1.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.1.linux-amd64.tar.gz
-    rm -f go1.19.1.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-    export GOPATH=/root/go
-    cat >> /root/.bashrc <<<"
-    export PATH=$PATH:/usr/local/go/bin
-    export GOPATH=/root/go"
-    mkdir $GOPATH
 }
 
 check_release_available() {
@@ -31,8 +21,9 @@ build_microshift() {
     local artifact_url=$(curl -Ls https://api.github.com/repos/redhat-et/microshift/releases | jq -r .[].tarball_url | grep $VERSION)
     curl -Lo "$VERSION.tar"  $artifact_url && tar -xf "$VERSION.tar"
     cd $(ls | grep microshift)
+    make clean
     make
-    mv microshift /usr/local/bin/microshift
+    sudo mv microshift /usr/local/bin/microshift
 }
 
 check_release_available
